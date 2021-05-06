3dac90e574b8fe26690dbe835b234cb0150fabe3
gemasnap <szabolcs.gema@snapsoft.hu>
2021-05-06 21:12:14 +0200

    Add sbject

--- Reframe/Reframe.Dal/Dto/AddSubject.cs
+++ Reframe/Reframe.Dal/Dto/AddSubject.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Reframe.Dal.Dto
 {
     public class AddSubject
     {
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
+        [StringLength(200, MinimumLength = 10)]
+        [Required]
         public string Description { get; set; }
+
+        [Range(1, 30)]
+        [Required]
         public int Credit { get; set; }
+
+        [Required]
         public int UserId { get; set; }
     }
 }
--- Reframe/Reframe.Web/Pages/Auth/AddSubject.cshtml.cs
+++ Reframe/Reframe.Web/Pages/Auth/AddSubject.cshtml.cs
@@ -1,16 +1,32 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Ganss.XSS;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Reframe.Dal.Dto;
+using Reframe.Dal.Services;
 
 namespace Reframe.Web.Pages.Auth
 {
     public class AddSubjectModel : PageModel
     {
-        public void OnGet()
+        private readonly SubjectService _subjectService;
+        public AddSubjectModel(SubjectService subjectService)
         {
+            _subjectService = subjectService;
+        }
+        [BindProperty]
+        public AddSubject AddSubject { get; set; }
+
+        public async Task<IActionResult> OnPostAddSubject()
+        {
+            var sanitizer = new HtmlSanitizer();
+           AddSubject.UserId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+           await  _subjectService.AddSubject(AddSubject);
+            return LocalRedirect("/");
         }
     }
 }
